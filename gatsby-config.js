a01b5d27ead777b17ab16c636e1017eb760517ae
Jonathan Lacasse <jonathanlacasse@gmail.com>
2022-05-01 01:50:05 -0400

    Update RSS name

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,7 +140,7 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "minijo.xyz RSS Feed",
           },
         ],
       },
